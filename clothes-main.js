6197089aee361eec17a844030ea8d16e12dcff57
ruige92 <32910854+ruige92@users.noreply.github.com>
2018-11-05 21:50:07 +0000

    testemail

--- clothes-main.js
+++ clothes-main.js
@@ -172,48 +172,59 @@ const siteScrolling=()=>{
 
 
 const contactPage=()=>{
-  // Get the form.
-  var form = $('#ajax-contact');
 
-  // Get the messages div.
-  var formMessages = $('#form-messages');
+    var form = $('#form'),
+        email = $('#email'),
+        subject = $('#subject'),
+        message = $('#message'),
+        info = $('#info'),
+        submit = $("#submit");
 
-  // Set up an event listener for the contact form.
-  $(form).submit(function(event) {
-    // Stop the browser from submitting the form.
-    event.preventDefault();
-    // Serialize the form data.
-    var formData = $(form).serialize();
-    // Submit the form using AJAX.
-    $.ajax({
-      type: 'POST',
-      url: $(form).attr('action'),
-      data: formData
-    }).done(function(response) {
-      // Make sure that the formMessages div has the 'success' class.
-      $(formMessages).removeClass('error');
-      $(formMessages).addClass('success');
+    form.on('input', '#email, #subject, #message', function() {
+      $(this).css('border-color', '');
+      info.html('').slideUp();
+    });
 
-      // Set the message text.
-      $(formMessages).text(response);
-
-      // Clear the form.
-      $('#name').val('');
-      $('#email').val('');
-      $('#message').val('');
-    }).fail(function(data) {
-      // Make sure that the formMessages div has the 'error' class.
-      $(formMessages).removeClass('success');
-      $(formMessages).addClass('error');
-
-      // Set the message text.
-      if (data.responseText !== '') {
-        $(formMessages).text(data.responseText);
-      } else {
-        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+    submit.on('click', function(e) {
+      e.preventDefault();
+      if(validate()) {
+        $.ajax({
+          type: "POST",
+          url: "mailer.php",
+          data: form.serialize(),
+          dataType: "json"
+        }).done(function(data) {
+          if(data.success) {
+            email.val('');
+            subject.val('');
+            message.val('');
+            info.html('Message sent!').css('color', 'green').slideDown();
+          } else {
+            info.html('Could not send mail! Sorry!').css('color', 'red').slideDown();
+          }
+        });
       }
-      });
-  });
+    });
+
+    function validate() {
+      var valid = true;
+      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+      if(!regex.test(email.val())) {
+        email.css('border-color', 'red');
+        valid = false;
+      }
+      if($.trim(subject.val()) === "") {
+        subject.css('border-color', 'red');
+        valid = false;
+      }
+      if($.trim(message.val()) === "") {
+        message.css('border-color', 'red');
+        valid = false;
+      }
+
+      return valid;
+    }
 };
 
 const main =()=>{
--- php/mailer.php
+++ php/mailer.php
@@ -1,17 +1,17 @@
 <?php
+if($_POST) {
+  $to = "fborui@gmail.com"; // your mail here
+  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+  $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+  $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+  $body = "Message: $message\nE-mail: $email";
 
-if (isset($_POST['submit'])){
-  $name = $_POST['name'];
-  $emailFrom = $_POST['email'];
-  $message = $_POST['message'];
-
-  $mailTo = "fborui@gmail.com";
-  $headers = "From: ".$emailFrom;
-  $txt = "You have received an e-mail from ".$name.".\n\n".$message;
-
-
-  mail($mailTo, $txt, $headers);
-  header("Location: index.php?mailsend");
+  if(@mail($to, $subject, $body)) {
+    $output = json_encode(array('success' => true));
+    die($output);
+  } else {
+    $output = json_encode(array('success' => false));
+    die($output);
+  }
 }
-
 ?>
